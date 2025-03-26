4f86fa29380806f3ce32b0f45f23c830db6eb0f2
nduggaraju83 <nduggaraju.oracle@gmail.com>
2025-03-26 20:52:08 +0530

    Create run_Validation.py
    LLM

---
+++ code/src/LLM/run_Validation.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+
+
+
+# Check if two arguments are provided
+if len(sys.argv) != 4:
+    print("Usage: python script.py <arg1> <arg2> <arg3>")
+    sys.exit(1)
+
+# Extract arguments
+rules_pdf = sys.argv[1]  # First argument
+data_csv = sys.argv[2]  # Second argument
+report_csv = sys.argv[3]  # Third argument
+
+# Define file paths
+#yaml_file = "validation_rules.yaml"
+#yaml_raw = "rules.yaml"
+#validation_results = "validation_results.csv"
+
+
+# Run scripts in sequence
+subprocess.run(["C:\\Narasimha\\Python\\PythonEnv\\Scripts\\python.exe", "PdfToCsv_New.py", "--rules_pdf", rules_pdf])
+subprocess.run(["C:\\Narasimha\\Python\\PythonEnv\\Scripts\\python.exe", "regExpollama.py"])
+subprocess.run(["C:\\Narasimha\\Python\\PythonEnv\\Scripts\\python.exe", "validateDataYaml.py", "--input_csv", data_csv,"--output_csv", report_csv])
+
+print("Pipeline execution completed.")
